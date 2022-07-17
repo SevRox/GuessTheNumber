c759a4ff26557d32dd93deb274e55119f33ba055
SevRox <misterpigbig@gmail.com>
2022-07-17 21:41:45 +0200

    Reminder of OOP, simple tests, preparing for adding complex solutions.

--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,71 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+
+//  Declarations
+    Random rnd = new Random();
+    private int miss=0;
+    private final int num = rnd.nextInt(99);
+
     public static void main(String[] args) {
 
 //      Declarations
+        Main obj = new Main();
         Scanner userInput = new Scanner(System.in);
-        Random rnd = new Random();
-        int guess, miss=0, num = rnd.nextInt(99);
+
+        int guess;
 
 //      Start of the game
-        System.out.println(num);
+        System.out.println(obj.getNum());
         System.out.println("Hello!\nGuess the number:");
         do{
             guess = userInput.nextInt();
+            obj.compere(guess);
 
-            if(guess > num){
-                System.out.println("Ops to high!\nGuess again:");
-                miss++;
-            }
+        }while(guess != obj.getNum());
+    }
+    // Methods for "cleaner" code (more like for fun)
 
-            else if(guess < num){
-                System.out.println("Ops to low!\nGuess again:");
-                miss++;
-            }
-            else {
-                System.out.println("You guessed!\nThe number is: " + num
-                        + "\nYou missed: " + miss + " times."
-                        + "\nYou win!");
-            }
-        }while(guess != num);
+    void incrementMiss(){
+        this.miss++;
+    }
+
+    int getMiss(){
+        return miss;
+    }
+
+    int getNum(){
+        return num;
+    }
+
+    void toHigh(){
+        System.out.println("Ops to high!\nGuess again:");
+    }
+
+    void toLow(){
+        System.out.println("Ops to low!\nGuess again:");
+    }
+
+    void match(){
+        System.out.println("You guessed!\nThe number is: " + getNum()
+                + "\nYou missed: " + getMiss() + " times."
+                + "\nYou win!");
+    }
+
+    void compere(int guess){
+        if(guess > getNum()){
+            toHigh();
+            incrementMiss();
+        }
+
+        else if(guess < getNum()){
+            toLow();
+            incrementMiss();
+        }
+
+        else{
+            match();
+        }
     }
 }
+
